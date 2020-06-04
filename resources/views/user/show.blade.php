8924355adb3ce5011816d1ed6fc2526f82189135
michael_gvdw <michael@fragrant-chios.com>
2020-06-04 15:12:40 +0200

    minor change

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,38 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col">
 
-        </div>
-    </div>
 
-    <div class="row">
-        <div class="col">
-<<<<<<< HEAD
-            <ul class="nav nav-pills mb-4" id="pills-tab" role="tablist">
-                <li class="nav-item">
-                  <a class="nav-link active" id="pills-home-tab" href="/course" role="tab" aria-controls="pills-home" aria-selected="true">Courses</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-profile-tab" href="/coach" role="tab" aria-controls="pills-profile" aria-selected="false">Coaches</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" href="/regatta" role="tab" aria-controls="pills-contact" aria-selected="false">Regattas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="pills-contact-tab" href="/article" role="tab" aria-controls="pills-contact" aria-selected="false">Articles</a>
-                  </li>
-              </ul>
-        </div>
-=======
 
-        </div>
 
         <div class="col-l">
             <ul class="nav nav-pills mb-4" id="pills-tab" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" id="pills-home-tab" href="/course" role="tab" aria-controls="pills-home" aria-selected="true">Courses</a>
+                    <a class="nav-link" id="pills-home-tab" href="/course" role="tab" aria-controls="pills-home" aria-selected="true">Courses</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" id="pills-profile-tab" href="/coach" role="tab" aria-controls="pills-profile" aria-selected="false">Coaches</a>
@@ -96,7 +72,5 @@
             </div>
         </div>
 
-
->>>>>>> master
     </div>
 @endsection
