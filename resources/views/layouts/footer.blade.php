3f476ce3a6967298b4978d9add8a2fa4d3991d0e
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-06-14 01:18:35 +0300

    Changed footer text

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-4 bg-dark text-white-50">
     <div class="container text-center">
-      <small>Copyright & copy; Your Website</small>
+      <small>Copyright © Kristiyan Strahilov, Michael Groenewegen van der Weijden, Nikola Bolic - 2020</small>
     </div>
 </footer>
 
